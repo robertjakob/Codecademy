d6799fa2e2b0883b08d7ecb40dd136217b3ce4bc
Robi-Wan Kenobi <jakob.robert@icloud.com>
2022-10-27 16:07:03 +0800

    Create python_functions.py

---
+++ python_functions.py
@@ -0,0 +1,13 @@
+# DATA SCIENTIST: MACHINE LEARNING SPECIALIST
+# Python Functions: Medical Insurance Project
+
+# Create calculate_insurance_cost() function below: 
+def calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
+    estimated_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+    print("The estimated insurance cost for " + name + " is " + str(estimated_cost) + " dollars.")
+    return estimated_cost
+
+maria_insurance_cost = calculate_insurance_cost(name = "Maria", age = 28, sex = 0, bmi = 26.2, num_of_children = 3, smoker = 0)
+
+omar_insurance_cost = calculate_insurance_cost(name = "Omar", age = 35, sex = 1, bmi = 22.2, num_of_children = 0, smoker = 1)
