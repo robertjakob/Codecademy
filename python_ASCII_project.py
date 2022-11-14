d9034e0b17a98cd07e49e6c238df68cc9efdd299
Robert Jakob <jakob.robert@icloud.com>
2022-11-14 13:07:23 +0700

    Create python_ASCII_project.py

---
+++ python_ASCII_project.py
@@ -0,0 +1,10 @@
+#LEARN PYTHON 3
+#Block Letters
+
+print("         ,MMM8&&&.")
+print("    _...MMMMM88&&&&..._")
+print(" .::'''MMMMM88&&&&&&'''::.")
+print("::     MMMMM88&&&&&&     ::")
+print("'::....MMMMM88&&&&&&....::'")
+print("   `''''MMMMM88&&&&''''`")
+print("         'MMM8&&&'")
