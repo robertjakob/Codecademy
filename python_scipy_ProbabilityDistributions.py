4a1125ab8299de32dd98f683202cc0e40e69dd23
Robert Jakob <jakob.robert@icloud.com>
2022-12-08 00:01:35 +0800

    Update python_scipy_ProbabilityDistributions.py

--- python_scipy_ProbabilityDistributions.py
+++ python_scipy_ProbabilityDistributions.py
@@ -48,3 +48,30 @@ print(1 - stats.norm.cdf(24, 20, 3))
 # temperature being between 21*C and 25*C
   # x = 24, loc = 20, scale = 3
 print(stats.norm.cdf(25, 20, 3) - stats.norm.cdf(21, 20, 3))
+
+
+# Further Practice Quesions from MORE ON PROBABILITY DISTRIBUTIONS
+
+# Practice Question 1: 
+# You work at ambulance dispatch where the number of calls that come in daily follows the Poisson distribution with lambda equal to 9. There’s a rule that a team can go on no more than 12 calls a day. But how often could this happen?
+# Create a variable calls that is the probability of observing more than 12 calls on a given day. Then print calls.
+calls = 1 - stats.poisson.cdf(12, 9)
+print(calls)
+
+# Practice Question 2: 
+# Let’s say that you have to call in a backup team if you have 10 or more calls in a given day. But you don’t want to have to call in a backup team unless they really will be needed. But what is the probability that they will be called and not needed?
+# Create and print a variable false_backup that is the probability of observing a minimum of 10 calls, but no more than 12. Then print false_backup.
+false_backup = stats.poisson.cdf(12, 9) - stats.poisson.cdf(9, 9)
+print(false_backup)
+
+# Practice Question 3: 
+# A certain tennis star has a first-serve rate of 62%. Let’s say they serve 80 times in a given match. What is the expected value of the number of serves they make?
+# Create and print a variable expected_serves that is the number of first-serves they are expected to make.
+expected_serves = 80*0.62
+print(expected_serves)
+
+# Practice Question 4
+# At the same first-serve rate, what is the variance of this player’s first-serves?
+# Create and print a variable variance_serves that is the variance of the player making their first serve.
+variance_serves = 80*0.62*(1-0.62)
+print(variance_serves)
